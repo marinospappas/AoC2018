d86c60ca8c92fff98c26b69f901aa9f90b2af292
marinos <marinosch.99981@gmail.com>
2023-09-03 19:08:44 +0200

    day 12 skeleton

--- src/main/kotlin/mpdev/springboot/aoc2018/solutions/day12/Day12.kt
+++ src/main/kotlin/mpdev/springboot/aoc2018/solutions/day12/Day12.kt
@@ -1,4 +1,4 @@
-package mpdev.springboot.aoc2018.solutions.day11
+package mpdev.springboot.aoc2018.solutions.day12
 
 import mpdev.springboot.aoc2018.model.PuzzlePartSolution
 import mpdev.springboot.aoc2018.solutions.PuzzleSolver
@@ -6,10 +6,10 @@ import org.springframework.stereotype.Component
 import kotlin.system.measureTimeMillis
 
 @Component
-class Day11: PuzzleSolver() {
+class Day12: PuzzleSolver() {
 
     final override fun setDay() {
-        day = 11
+        day = 12
     }
 
     init {
@@ -17,27 +17,21 @@ class Day11: PuzzleSolver() {
     }
 
     var result = ""
-    lateinit var fuelCellGrid: FuelCellGrid
 
     override fun initSolver(): Pair<Long,String> {
         val elapsed = measureTimeMillis {
-            fuelCellGrid = FuelCellGrid(inputData)
         }
         return Pair(elapsed, "milli-sec")
     }
 
     override fun solvePart1(): PuzzlePartSolution {
         val elapsed = measureTimeMillis {
-            val (maxPower3Coord, _) = fuelCellGrid.findHighestPowerSubGrid()
-            result = "${maxPower3Coord.x},${maxPower3Coord.y}"
         }
         return PuzzlePartSolution(1, result, elapsed)
     }
 
     override fun solvePart2(): PuzzlePartSolution {
         val elapsed = measureTimeMillis {
-            val (maxPowerCoord, size, _) = fuelCellGrid.findAnyHighestPowerSubGrid()
-            result = "${maxPowerCoord.x},${maxPowerCoord.y},$size"
         }
         return PuzzlePartSolution(2, result, elapsed)
     }
---
+++ src/test/kotlin/mpdev/springboot/aoc2018/day12/Day12Test.kt
@@ -0,0 +1,53 @@
+package mpdev.springboot.aoc2018.day12
+
+import mpdev.springboot.aoc2018.input.InputDataReader
+import mpdev.springboot.aoc2018.solutions.day12.Day12
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import java.awt.Point
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class Day12Test {
+
+    private val day = 12                                     ///////// Update this for a new dayN test
+    private val puzzleSolver = Day12()                      ///////// Update this for a new dayN test
+    private val inputDataReader = InputDataReader("src/test/resources/inputdata/input")
+    private var inputLines: List<String> = inputDataReader.read(day)
+
+    @BeforeEach
+    fun setup() {
+        puzzleSolver.setDay()
+        puzzleSolver.inputData = inputLines
+        puzzleSolver.initSolver()
+    }
+
+    @Test
+    @Order(1)
+    fun `Sets Day correctly`() {
+        assertThat(puzzleSolver.day).isEqualTo(day)
+    }
+
+    @Test
+    @Order(2)
+    fun `Reads Input and sets up `() {
+
+    }
+
+    @Test
+    @Order(4)
+    fun `Solves Part 1`() {
+        assertThat(puzzleSolver.solvePart1().result).isEqualTo("")
+    }
+
+    @Test
+    @Order(7)
+    fun `Solves Part 2`() {
+        assertThat(puzzleSolver.solvePart2().result).isEqualTo("")
+    }
+
+}
